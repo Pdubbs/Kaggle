0eba2d73efe5568b06e5c36653f558cb5c07147d
Pdubbs <mr.wyatt@gmail.com>
2015-04-06 10:45:42 -0500

    Updates to lasso model; running modular and using the proper weather set

--- Walmart/weather_cleaning.R
+++ Walmart/weather_cleaning.R
@@ -1,4 +1,5 @@
 library(lubridate)
+library(plyr)
 
 weather <- read.csv("weather.csv")
 #recode trace amounts as a small number
@@ -8,6 +9,7 @@ weather$preciptotal <- as.character(weather$preciptotal)
 weather$preciptotal[weather$preciptotal=="T"] <- .004
 weather$depart_missing <- 0
 weather$depart_missing[weather$depart=="M"] <- 1
+weather$date<-ymd(weather$date)
 
 #really shitty imputation; factor values get recoded as character and numeric, then the NA values (where there was a value that could not get encoed as numeric) get replaced by means
 for(i in c(3:12,14:18,20)){
@@ -15,6 +17,31 @@ for(i in c(3:12,14:18,20)){
   weather[is.na(weather[,i]),i] <- mean(weather[,i],na.rm=TRUE)
 }
 
-weather$month <- factor(month(mdat$date))
-weather$wday <- wday(mdat$date) #weekend shopping spree!
+weather$month <- factor(month(weather$date))
+weather$wday <- wday(weather$date) #weekend shopping spree!
 
+#variables about the future forecast:
+weather$nw_precip<-
+  apply(weather,1,function(x) { 
+    sum(weather$preciptotal[weather$date > ymd(x["date"]) & 
+                              weather$date < (ymd(x["date"])+days(7)) &
+                              weather$station_nbr == as.numeric(x["station_nbr"])] 
+                            )}
+    )
+
+weather<-ddply(weather,.(date,station_nbr),mutate, 
+         nw_precip = sum(weather$preciptotal[weather$date > date &
+                                               weather$date < (date+days(7)) &
+                                               weather$station_nbr == station_nbr]),
+         nw_snow = sum(weather$snowfall[weather$date > date &
+                                          weather$date < (date+days(7)) &
+                                          weather$station_nbr == station_nbr]),
+         nw_high = max(weather$tmax[weather$date > date &
+                                      weather$date < (date+days(7)) &
+                                      weather$station_nbr == station_nbr]),
+         nw_low = sum(weather$tmin[weather$date > date &
+                                      weather$date < (date+days(7)) &
+                                      weather$station_nbr == station_nbr])
+         )
+
+weather$nw_high[!is.finite(weather$nw_high)]<-mean(weather$nw_high[is.finite(weather$nw_high)],na.rm=TRUE)
